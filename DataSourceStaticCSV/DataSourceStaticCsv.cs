0255749667db9b6b1a553d91dfe9788f2780a2be
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-08-01 00:08:27 -0500

    Added another Data Driven Test using a preloaded Csv File All the data is in a single Csv file Checks Three Comparisions Are the values Equal Is the Sum calculated properly Is the Multiplication calculated properly

---
+++ DataSourceStaticCSV/DataSourceStaticCsv.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataSourceStaticCSV
+{
+    [TestClass]
+    public class DataSourceStaticCsv
+    {
+        public TestContext TestContext { get; set; }
+
+        private int _left = 0;
+        private int _right = 0;
+        private bool _areEqual = false;
+        private int _sum = 0;
+        private int _mul = 0;
+
+        [ClassInitialize]
+        public static void ClassInitializer(TestContext testContext)
+        {
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "ExampleInputData.csv",
+            "ExampleInputData#csv",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("ExampleInputData.csv")]
+        [TestMethod]
+        public void AreEqual()
+        {
+            _left = (int)TestContext.DataRow[0];
+            _right = (int)TestContext.DataRow[1];
+            _areEqual = (int)TestContext.DataRow[2] != 0;
+
+            Assert.AreEqual(_left == _right, _areEqual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "ExampleInputData.csv",
+            "ExampleInputData#csv",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("ExampleInputData.csv")]
+        [TestMethod]
+        public void SumTest()
+        {
+            _left = (int)TestContext.DataRow[0];
+            _right = (int)TestContext.DataRow[1];
+            _sum = (int)TestContext.DataRow[3];
+
+            Assert.AreEqual(_left + _right, _sum);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "ExampleInputData.csv",
+            "ExampleInputData#csv",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("ExampleInputData.csv")]
+        [TestMethod]
+        public void MulTest()
+        {
+            _left = (int)TestContext.DataRow[0];
+            _right = (int)TestContext.DataRow[1];
+            _mul = (int)TestContext.DataRow[4];
+
+            Assert.AreEqual(_left * _right, _mul);
+        }
+    }
+}
---
+++ DataSourceStaticCSV/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataSourceStaticCSV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataSourceStaticCSV")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65ed9264-9704-4445-bd84-615768fed66a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
